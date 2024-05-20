fe554143701edc6bd3126d15d7690ce421324e09
hyeonw8 <whgusrud223@gmail.com>
2024-05-20 15:51:53 +0900

    계산기 타임어택

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,48 @@
+import { useState } from "react";
+
 export default function App() {
+  const [result, setResult] = useState(0);
+  const [value, setValue] = useState(0);
+
+  const addNumber = () => {    
+    if(!value.trim()) {
+      alert('숫자 입력해주세요');
+      return;
+    }
+
+    setResult(value + result);
+    setValue('');
+  }
+
+  const minusNumber = () => {
+    if(!value.trim()) {
+      alert('숫자 입력해주세요');
+      return;
+    }
+
+    setResult(value - result);
+    setValue('');
+    return 
+  }
+
+  const resetNumber = () => {
+    setResult('');
+  }
+
   return (
     <div>
       <h1>덧셈과 뺄셈이 가능한 앱 만들기</h1>
       <div>
-        <input /> 만큼을 <button>더할게요</button> <button>뺄게요</button>
-        <button>초기화</button>
+        <input value={value} onChange={(e) => setValue(e.target.value)} />
+         만큼을 
+         <button onClick={() => addNumber}>더할게요</button> 
+         <button onClick={() => minusNumber}>뺄게요</button>
+        <button onClick={() => resetNumber}>초기화</button>
       </div>
       <hr />
       <div>
         <h3>결과</h3>
-        <p>0</p>
+        <p>{result}</p>
       </div>
     </div>
   );
